4a31de7506be5d91fb0cade83c45e0c48b7cf073
Juliano Lima <julianodgtz@gmail.com>
2020-01-17 13:42:22 -0300

    Adds destructuring function

--- clojure-noob/src/clojure_noob/core.clj
+++ clojure-noob/src/clojure_noob/core.clj
@@ -1,7 +1,18 @@
 (ns clojure-noob.core
   (:gen-class))
 
+(defn announce-treasure-location
+[{lat :lat lng :lng}]
+(println (str "Treasure lat:" lat)
+(println (str "Treasure lgn" lng
+
+
+))))
+
+
 (defn -main
   "I don't do a whole lot ... yet."
-  [& args]
+  [& args] 
+  (announce-treasure-location {:lat 28.22 :lng 81.33})
+
   (println "Hello, World!"))
